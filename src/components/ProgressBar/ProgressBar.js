f0d9cc8ecaf975a48ba6d097007a62f794ccef69
Martin Yrjölä <martin.yrjola@gmail.com>
2021-11-14 10:43:18 +0200

    Finalize progress bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -8,10 +8,22 @@ const Wrapper = styled.div`
   min-width: 200px;
   box-shadow: inset 0 2px 4px rgba(128, 128, 128, 0.35);
   background-color: ${COLORS.transparentGray15};
+  border-radius: var(--border-radius);
+  padding: var(--padding);
+  overflow: hidden;
+`
+
+const BarWrapper = styled.div`
+  height: var(--height);
+  border-radius: var(--inner-border-radius);
+  overflow: hidden;
 `
 
 const Bar = styled.div`
-  height: var(--height);
+  background-color: ${COLORS.primary};
+  height: 100%;
+  width: ${props => props.value}%;
+  transition: width 1s ease-in-out;
 `
 
 const maxProgress = 100
@@ -19,19 +31,26 @@ const maxProgress = 100
 const sizes = {
   small: {
     "--height": "8px",
+    "--border-radius": "4px",
   },
   medium: {
     "--height": "12px",
+    "--border-radius": "4px",
   },
   large: {
     "--height": "16px",
+    "--border-radius": "8px",
+    "--inner-border-radius": "4px",
+    "--padding": "4px"
   }
 }
 
 const ProgressBar = ({ value, size }) => {
   const progress = `Progressed ${value} of ${maxProgress}.`
-  return <Wrapper role="progressbar" aria-valuenow={value} aria-valuemin="0" aria-valuetext={progress} aria-valuemax={maxProgress} >
-    <Bar style={sizes[size]} />
+  return <Wrapper role="progressbar" style={sizes[size]} aria-valuenow={value} aria-valuemin="0" aria-valuetext={progress} aria-valuemax={maxProgress} >
+    <BarWrapper>
+      <Bar value={value} />
+    </BarWrapper>
   </Wrapper>
 };
 
