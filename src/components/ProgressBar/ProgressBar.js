a526926365242c3587617e1ba8a30fd6401f258e
Martin Yrjölä <martin.yrjola@gmail.com>
2021-11-14 10:29:19 +0200

    Add progress bar basics

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -3,10 +3,36 @@ import React from 'react';
 import styled from 'styled-components';
 
 import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+
+const Wrapper = styled.div`
+  min-width: 200px;
+  box-shadow: inset 0 2px 4px rgba(128, 128, 128, 0.35);
+  background-color: ${COLORS.transparentGray15};
+`
+
+const Bar = styled.div`
+  height: var(--height);
+`
+
+const maxProgress = 100
+
+const sizes = {
+  small: {
+    "--height": "8px",
+  },
+  medium: {
+    "--height": "12px",
+  },
+  large: {
+    "--height": "16px",
+  }
+}
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  const progress = `Progressed ${value} of ${maxProgress}.`
+  return <Wrapper role="progressbar" aria-valuenow={value} aria-valuemin="0" aria-valuetext={progress} aria-valuemax={maxProgress} >
+    <Bar style={sizes[size]} />
+  </Wrapper>
 };
 
 export default ProgressBar;
