f107ea63049d4429c74bbe6f9b39625e8f962369
Martin Yrjölä <martin.yrjola@gmail.com>
2021-11-14 14:24:31 +0200

    Implement IconInput

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,6 +6,65 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
+const Wrapper = styled.div`
+  position: relative;
+  display: flex;
+  width: ${props => props.$width}px;
+  align-items: center;
+  padding-bottom: var(--padding-bottom);
+  font-family: 'Roboto', sans-serif;
+  font-size: var(--font-size);
+
+  &:focus-within {
+    outline: 2px solid #4374CB;
+    outline-offset: 4px;
+    border-radius: 2px;
+  }
+`
+
+const Input = styled.input`
+  all: unset;
+  
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  padding-left: var(--padding-left);
+  color: ${COLORS.gray700};
+  
+  &::placeholder {
+    color: ${COLORS.gray500}
+  }
+  
+  &:hover {
+    color: ${COLORS.black};
+  }
+`
+
+const BottomBar = styled.div`
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  border: var(--border-width) solid ${COLORS.black};
+  border-radius: var(--border-width);
+`
+
+const styles = {
+    small: {
+        '--border-width': '0.5px',
+        '--padding-bottom': '4px',
+        '--padding-left': '24px',
+        '--font-size': `${14 / 16}rem`
+    },
+    large: {
+        '--border-width': '1px',
+        '--padding-bottom': '8px',
+        '--padding-left': '36px',
+        '--font-size': `${18 / 16}rem`
+    }
+}
+
 const IconInput = ({
   label,
   icon,
@@ -13,7 +72,12 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
+  return <Wrapper $width={width} style={styles[size]}>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      <Input placeholder={placeholder} />
+      <Icon id={icon} size={size === 'large' ? 24 : 16} />
+      <BottomBar />
+  </Wrapper>;
 };
 
 export default IconInput;
